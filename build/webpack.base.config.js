05869f9ddf953fed3e2053681aa1962b91c95b41
Adel Dubinnikova <adelinyshka@gmail.com>
2019-03-21 00:38:38 +0300

    add icons & make mixin in table

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -2,6 +2,19 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+
+module.exports = {
+  plugins: [new DuplicatePackageCheckerPlugin()]
+};
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+}
 
 const PATHS = {
   src: path.join(__dirname, '../src'),
@@ -50,7 +63,7 @@ module.exports = {
         name: '[name].[ext]'
       }
     }, {
-      test: /\.scss$/,
+      test:  /\.(sass|scss)$/,
       use: [
         'style-loader',
         MiniCssExtractPlugin.loader,
