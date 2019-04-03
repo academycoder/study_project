94d137e160b768162f9e23fab75c95e1d19b95af
Adel Dubinnikova <adelinyshka@gmail.com>
2019-04-03 22:51:01 +0300

    add paddings & 1st block in pixel perfect

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
 
 module.exports = {
@@ -20,7 +20,7 @@ const PATHS = {
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
   assets: 'assets/'
-}
+};
 
 module.exports = {
   // BASE config
@@ -106,5 +106,5 @@ module.exports = {
       { from: `${PATHS.src}/static`, to: '' },
     ])
   ],
-}
+};
 
